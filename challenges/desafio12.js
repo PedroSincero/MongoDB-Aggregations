4dfb3d0e9c3ed41a35e189a3c6844280c3e449bb
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-20 15:50:04 -0300

    REQ12 - Adicionando requisito 12 - [DONE]

---
+++ challenges/desafio12.js
@@ -0,0 +1,34 @@
+db.trips.aggregate([{
+    $addFields: {
+      days: {
+        $dayOfWeek: "$startTime"
+      }
+    }
+  },
+  {
+    $group: {
+      _id: {
+        dia_semana: "$days",
+        estacao: "$startStationName"
+      },
+      total: {
+        $sum: 1
+      },
+    }
+  },
+  {
+    $sort: {
+      total: -1
+    }
+  },
+  {
+    $limit: 1
+  },
+  {
+    $project: {
+      nomeEstacao: "$_id.estacao",
+      total: "$total",
+      _id: false
+    }
+  }
+]);
